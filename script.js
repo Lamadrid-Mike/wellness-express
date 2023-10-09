c2d591261d48f8f2f75b25fedbd0aa788110105d
Lamadrid-Mike <mikealamadrid@hotmail.com>
2023-10-08 20:23:18 -0400

    files edited

--- script.js
+++ script.js
@@ -2,10 +2,19 @@ let mobileBtn = document.querySelector(".open__mobile--navigation");
 let mobileNavbar = document.querySelector(".navigation");
 let closeBtnMobile = document.querySelector(".navigation__close--btn");
 
+let anchorLinks = Array.from(
+  document.querySelectorAll(".navigation__overlay--content a")
+);
+
 mobileBtn.addEventListener("click", function () {
   mobileNavbar.style.width = "0%";
+  anchorLinks.forEach((el) => (el.style.opacity = "1"));
+
   mobileBtn.classList.toggle("change");
+
   if (mobileBtn.classList.contains("change")) {
     mobileNavbar.style.width = "100%";
+  } else {
+    anchorLinks.forEach((el) => (el.style.opacity = "0"));
   }
 });
