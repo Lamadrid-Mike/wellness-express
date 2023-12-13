2d1c5e28e8af92673c59dbdaa699c0027c1bd18c
Lamadrid-Mike <mikealamadrid@hotmail.com>
2023-12-12 22:56:59 -0500

    creating slideshow

--- script.js
+++ script.js
@@ -42,4 +42,55 @@ let commentRating = document.querySelector(
   ".testimonial__comment-ratings"
 ).children;
 
-console.log(commentSection);
+const comments = [
+  {
+    commentHeader: "Great service",
+    comment: "Great support such as time server. highly recommended",
+    rating: 4,
+    image: "url",
+    username: "Yves Tanguy",
+  },
+
+  {
+    commentHeader: "Love it",
+    comment: "Lovely people and great service",
+    rating: 5,
+    image: "url",
+    username: "Lady Barrera",
+  },
+];
+
+const ratingNumber = (number) => {
+  for (let i = 0; i < number; i++) {
+    const element = commentRating[i];
+    element.classList.add("checked");
+  }
+};
+
+const renderComments = (array) => {
+  for (let i = 0; i < array.length; i++) {
+    const element = array[i];
+
+    const { commentHeader, comment, img, rating, username } = element;
+
+    const html = `
+  <div class="testimonial__comment-section">
+    <h3>${commentHeader}</h3>
+    <p>${comment}</p>
+      <div class="testimonial__comment-ratings">
+        <span class="fa fa-star"></span>
+        <span class="fa fa-star"></span>
+        <span class="fa fa-star"></span>
+        <span class="fa fa-star"></span>
+        <span class="fa fa-star"></span>
+      </div>
+    <div src="" alt="" class="comment__user-img"></div>
+  <p class="comment__user-comment">${username}</p>
+ </div>
+`;
+
+    commentSection.insertAdjacentHTML("afterend", html);
+  }
+};
+
+renderComments(comments);
